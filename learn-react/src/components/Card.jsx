5fd7236696e8c99a4f7e3ed999347a0344536a5e
Oktavari Budi Arief <oktavariba@gmail.com>
2024-10-16 21:35:00 +0700

    nambah tentang componets

--- learn-react/src/components/Card.jsx
+++ learn-react/src/components/Card.jsx
@@ -1,46 +1,36 @@
 function Card(prop) {
-    const {children} = prop
-    return (
-        <div className="shadow-lg rounded-xl bg-white">{children}</div>
-    )
+    const { children } = prop;
+    return <div className='shadow-lg rounded-xl bg-white'>{children}</div>;
 }
 
-function Title(prop){
-    const {children} = prop
+function Title(prop) {
+    const { children } = prop;
 
     return (
-
-        <div className="p-4 border-b">
-            <h1 className="text-2xl font-semibold">{children}</h1>
+        <div className='p-4 border-b'>
+            <h1 className='text-2xl font-semibold'>{children}</h1>
         </div>
-    )
+    );
 }
 
-function Body(prop){
-    const {children} = prop
+function Body(prop) {
+    const { children } = prop;
+
+    return <div className='leading-relaxed p-4'>{children}</div>;
+}
+
+function Footer(prop) {
+    const { children } = prop;
 
     return (
-
-        <div className="leading-relaxed p-4">
+        <div className=' flex justify-center p-4 border-t' type='register'>
             {children}
         </div>
-    )
+    );
 }
 
-function Footer(prop){
-    const {children} = prop
-
-    return (
-
-        <div className=" flex justify-center p-4 border-t" type="register">
-            {children}
-        </div>
-    )
-}
-
-
-Card.Title = Title
-Card.Body = Body
-Card.Footer = Footer
+Card.Title = Title;
+Card.Body = Body;
+Card.Footer = Footer;
 
 export default Card;
